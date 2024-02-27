5ba69f44120b797933d7b9a79ad8af183b81406e
Matti J. Karki <mjk@iki.fi>
2024-02-27 22:23:58 +0200

    Koodin refaktorointia luettavammaksi

--- main.go
+++ main.go
@@ -17,6 +17,12 @@ type InitiativeStruct interface {
 	[]Initiative | InitiativeInfo
 }
 
+type Arguments struct {
+	help bool
+	all  bool
+	text string
+}
+
 func getData(url string) []byte {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -38,26 +44,57 @@ func getInitiativeStructFromJSON[T InitiativeStruct](data []byte) T {
 	return result
 }
 
+func parseArguments() Arguments {
+	var args Arguments
+	args.help = false
+	args.all = false
+	args.text = ""
+
+	if len(os.Args) < 2 {
+		args.help = true
+	}
+
+	for _, arg := range os.Args[1:] {
+		switch l_arg := strings.ToLower(arg); l_arg {
+		case "-h":
+			args.help = true
+		case "--help":
+			args.help = true
+		case "-a":
+			args.all = true
+		case "--all":
+			args.all = true
+		default:
+			args.text += l_arg + " "
+		}
+	}
+	args.text = strings.TrimRight(args.text, " ")
+
+	return args
+}
+
 func main() {
 	var data []byte
 	var aloitteet []Initiative = []Initiative{}
-	var listall = false
+	var kaikki = false
 	var offset = 0
 	var nimi string
 
-	if len(os.Args) < 2 {
+	args := parseArguments()
+
+	if args.help {
 		fmt.Println("Käyttö:")
-		fmt.Println("    " + os.Args[0] + ` "hakuteksti"`)
-		fmt.Println("  tai")
-		fmt.Println("    " + os.Args[0] + ` -a`)
+		fmt.Println("  " + os.Args[0] + ` [-h | --help | -a | --all] ["hakuteksti" ...]`)
+		fmt.Println()
+		fmt.Println(`  "hakuteksti"  etsii hakutekstiä aloitteiden otsikoista`)
+		fmt.Println(`  -h | --help   tulostaa tämän ohjeen`)
+		fmt.Println(`  -a | --all    tulostaa kaikki kansalaisaloitteet`)
 		os.Exit(0)
 	}
 
-	search := os.Args[1]
-
-	if search == "-a" {
+	if args.all {
 		fmt.Println("Listataan kaikki aloitteet:")
-		listall = true
+		kaikki = true
 	}
 
 	data = getData(fmt.Sprintf("%s%d", URL, offset))
@@ -74,11 +111,11 @@ func main() {
 		} else {
 			nimi = initiative.Name["fi"].(string)
 		}
-		if listall {
+		if kaikki {
 			fmt.Println("- " + nimi)
 		} else {
 			nimi_l := strings.ToLower(nimi)
-			if strings.Contains(nimi_l, search) {
+			if strings.Contains(nimi_l, args.text) {
 				url := initiative.ID
 				data = getData(url)
 				initiativeStruct := getInitiativeStructFromJSON[InitiativeInfo](data)
